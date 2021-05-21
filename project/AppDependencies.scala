72e8b38d210632d9cc853d481804ef3353483205
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2021-05-21 15:05:49 +0100

    #NOJIRA Update to latest library versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-backend-play-28"  % "5.2.0",
+    "uk.gov.hmrc"           %% "bootstrap-backend-play-28"  % "5.3.0",
     "uk.gov.hmrc"           %% "play-language"              % "5.0.0-play-28",
     "com.github.tototoshi"  %% "scala-csv"                  % "1.3.7"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28" % "5.1.0"         % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28" % "5.3.0"         % Test,
     "org.scalatest"           %% "scalatest"              % "3.2.8"         % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"     % "5.1.0"         % Test,
     "com.typesafe.play"       %% "play-test"              % current         % Test,
